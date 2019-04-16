38b306d3f0389441b4bb442d5859ddac10c61b4b
zhengxiaochen <hellozhengxiaochen@gmail.com>
2019-04-16 15:31:46 +0200

    Update mam_publish.js
    changed address to public iota nodes. Added logging function to record waiting time for all 100 tests.

--- tremor_mam/mam_publish.js
+++ tremor_mam/mam_publish.js
@@ -14,7 +14,6 @@ Any future use please give credits to the original author too.
 The original code can be found here:
 https://www.mobilefish.com/developer/iota/iota_quickguide_raspi_mam.html 
 */
-
 const Mam = require('./lib/mam.client.js');
 const IOTA = require('iota.lib.js');
 //const moment = require('moment');
@@ -22,8 +21,8 @@ const IOTA = require('iota.lib.js');
 //https://www.tangle-nodes.com/?sorts[load]=1&sorts[tls]=-1 
 //const iota = new IOTA({ provider: 'https://nodes.testnet.iota.org:443' }); 
 //const iota = new IOTA({ provider: 'https://irino.de:443' });
-const iota = new IOTA({ provider: 'https://power.benderiota.com:14267' }); //memory usage:17.9%, neighbours: 7
-const MODE = 'public'; // public, private or restricted
+const iota = new IOTA({ provider: 'https://pow.iota.community:443' }); //memory usage:17.9%, neighbours: 7
+const MODE = 'restricted'; // public, private or restricted
 const SIDEKEY = 'mysecret'; // Enter only ASCII characters. Used only in restricted mode
 const SECURITYLEVEL = 3; // 1, 2 or 3
 //const TIMEINTERVAL  = 30; // seconds
@@ -62,31 +61,36 @@ const executeDataPublishing = async function() {
 	 const jsonpath = "./test_samples/"; //path to the folder where JSON files saved
 	 
 	 var i;	
+	 var t_array =[]; //array to hold waiting time
     for (i = 1; i < 101; i++) {
        var full_path = jsonpath.concat("js",i,".json"); //full path to the JSON file           
        var textByLine = fs.readFileSync(full_path).toString().split("\n"); //parse JSON object
-       const json = JSON.parse(textByLine[0]); 
-       //json = json_multi[0]; //one object each time in this case
+       const json_multi = JSON.parse(textByLine[0]); 
+       json = json_multi[0]; //one object each time in this case
        //console.log("json=",json); //print json object
               
        //count waiting time
        var date0 = new Date();
        var t0 = date0.getTime();             
        const root = await publish(json); //publish data
+       //const root = publish(json);
        var date1 = new Date();
        var t1 = date1.getTime();       
-       var watingtime = t1-t0;
+       var waitingtime = t1-t0;
               
-       //print results  "t":20190110081500,"tre:":0
-       console.log(`waiting_time:${watingtime}`); 
-       console.log(`t: ${json.t}, tre: ${json.tre}`);
+       t_array[i-1] = waitingtime;
+        
+       //print results 
+       console.log(`waiting_time:${waitingtime}`); 
+       console.log(t_array.toString()); 
+       //console.log(`date: ${json.date}, alcohol: ${json.alcohol}, caffee: ${json.caffee},Medicine: ${json.Medicine}, t1: ${json.t1}, ArmExtendL: ${json.ArmExtendL}, ArmExtendR: ${json.ArmExtendR},t2: ${json.t2}, TouchNoseL: ${json.TouchNoseL}, TouchNoseR: ${json.TouchNoseR}, t3: ${json.t3}, Writting: ${json.Writting}, t4: ${json.t4}, LargeSpiral: ${json.LargeSpiral}`);
+      //"X":18,"date":"2016-05-30T17:23:56.000Z","alcohol":"No","caffee":"Si","Medicine":"SumialMysoline","t1":"2016-05-30T17:23:37.000Z","ArmExtendL":0,"ArmExtendR":4,"t2":"2016-05-30T17:23:39.000Z","TouchNoseL":0,"TouchNoseR":4,"t3":"2016-05-30T17:23:41.000Z","Writting":4,"t4":"2016-05-30T17:23:43.000Z","LargeSpiral":4,"SmallSpiral":0,"StraightLine":0,"t5":"2016-05-30T17:23:45.000Z","CupsL":0,"CupsR":4,"t6":"2016-05-30T17:23:46.000Z","DrinkingL":4,"t7":"2016-05-30T17:23:48.000Z","Angle":0,"PhoneMod":"Redmi 3","MAC":"64:cc:2e:d6:6d:46","uuid":"784c8bcbeed07c30"
     }  	
 }
 
 // Start main process
 executeDataPublishing();
 
-//ROOT IN THIS CASE: XWLWVYLVBJHLLSFBWFJMHGWAELURPQRXKBOXOXIKJNGKIVODXZPCBQ9LMMIFPTFICTHPQKMECQSHFGV9V
 
 //set interval of broadcasting
 //setInterval(executeDataPublishing, TIMEINTERVAL*1000); 
